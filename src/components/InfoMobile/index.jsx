1dd3c36cee69aa7166160f21198ab18dd614684a
lauradiaz <lauradiaz1586@gmail.com>
2022-06-29 10:51:31 +0400

    add key to InfoMobile children components

--- src/components/InfoMobile/index.jsx
+++ src/components/InfoMobile/index.jsx
@@ -1,45 +1,15 @@
-import {
-  Accordion,
-  PressOnline,
-  Press,
-  Articles,
-  Exhibitions,
-  About,
-  Contact,
-  Awards,
-} from '@/components/index';
+import { Accordion, PressOnline, Press, Articles, Exhibitions, About, Contact, Awards } from "@/components/index";
 
-export default function InfoMobile({
-  press,
-  articles,
-  exhibitions,
-  awards,
-  pressOnline,
-}) {
+export default function InfoMobile({ press, articles, exhibitions, awards, pressOnline }) {
   return (
     <div className='info-mobile-wrapper'>
       <Accordion title='About' content={<About />} />
       <Accordion title='Contact' content={<Contact />} />
-      <Accordion
-        title='Press'
-        content={[
-          <Press data={press} title='Print (selected):' />,
-          <PressOnline data={pressOnline} title='Print Online (selected):' />,
-        ]}
-      />
+      <Accordion title='Press' content={[<Press data={press} title='Print (selected):' key={press[0].id} />, <PressOnline data={pressOnline} title='Print Online (selected):' key={pressOnline[1].id} />]} />
 
-      <Accordion
-        title='Awards & Distinctions'
-        content={<Awards data={awards} />}
-      />
-      <Accordion
-        title='Research & Articles'
-        content={<Articles data={articles} />}
-      />
-      <Accordion
-        title='Exhibitions'
-        content={<Exhibitions data={exhibitions} />}
-      />
+      <Accordion title='Awards & Distinctions' content={<Awards data={awards} />} />
+      <Accordion title='Research & Articles' content={<Articles data={articles} />} />
+      <Accordion title='Exhibitions' content={<Exhibitions data={exhibitions} />} />
     </div>
   );
 }
