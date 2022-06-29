a4394070fb4f2c1ed6f855f16ca3f8b7353e4099
lauradiaz <lauradiaz1586@gmail.com>
2022-06-29 10:44:54 +0400

    add new project

--- src/components/CoverImg/index.jsx
+++ src/components/CoverImg/index.jsx
@@ -3,25 +3,11 @@ export default function CoverImg({ element }) {
   return (
     <div className='main-image'>
       {element.mp4 ? (
-        <video
-          loop
-          autoPlay
-          muted
-          playsinline
-          webkit-playsinline
-          controlsList='nofullscreen'
-          className='video-main-image'
-        >
+        <video loop autoPlay muted playsinline webkit-playsinline controlsList='nofullscreen' className='video-main-image'>
           <source src={element.mp4Slug} type='video/mp4' />
         </video>
       ) : (
-        <img
-          alt='graphic-design'
-          src={element.imageSlag}
-          className='graphic-design-img'
-          // width='800px'
-          // height='534px'
-        />
+        <img alt='graphic-design' src={element.imageSlag} className='graphic-design-img' />
       )}
     </div>
   );
